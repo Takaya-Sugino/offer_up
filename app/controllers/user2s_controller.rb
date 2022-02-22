e1f611d2dade8e13700d12f6d53c7eb875b96d73
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-02-22 03:52:32 +0000

    Remove controller comments

--- app/controllers/user2s_controller.rb
+++ app/controllers/user2s_controller.rb
@@ -1,27 +1,22 @@
 class User2sController < ApplicationController
   before_action :set_user2, only: %i[show edit update destroy]
 
-  # GET /user2s
   def index
     @q = User2.ransack(params[:q])
     @user2s = @q.result(distinct: true).includes(:transactions,
                                                  :sellers).page(params[:page]).per(10)
   end
 
-  # GET /user2s/1
   def show
     @transaction = Transaction.new
   end
 
-  # GET /user2s/new
   def new
     @user2 = User2.new
   end
 
-  # GET /user2s/1/edit
   def edit; end
 
-  # POST /user2s
   def create
     @user2 = User2.new(user2_params)
 
@@ -32,7 +27,6 @@ class User2sController < ApplicationController
     end
   end
 
-  # PATCH/PUT /user2s/1
   def update
     if @user2.update(user2_params)
       redirect_to @user2, notice: "User2 was successfully updated."
@@ -41,7 +35,6 @@ class User2sController < ApplicationController
     end
   end
 
-  # DELETE /user2s/1
   def destroy
     @user2.destroy
     redirect_to user2s_url, notice: "User2 was successfully destroyed."
@@ -49,12 +42,10 @@ class User2sController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user2
     @user2 = User2.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def user2_params
     params.require(:user2).permit(:name)
   end
